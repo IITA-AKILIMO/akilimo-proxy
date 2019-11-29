c3729d79131e5d903a4d6dea4156c61a55a85ce8
Sammy Barasa <barsamms@gmail.com>
2019-11-29 09:49:09 +0300

    Testing

--- clean-containers.sh
+++ clean-containers.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-docker run \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $PWD/conf/traefik.local.toml:/traefik.toml \
-  -v $PWD/conf/acme.json:/acme.json \
-  -p 80:80 \
-  -l traefik.frontend.rule=Host:monitor.tsobu.com \
-  -l traefik.port=8080 \
-  --network web \
-  --name traefik \
-  traefik:1.7.2-alpine
+docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
