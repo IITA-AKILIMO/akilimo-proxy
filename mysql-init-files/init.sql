d88e3ab9c22ce17bd1c214b65b8daab0f9aab476
Sammy Barasa <barsamms@gmail.com>
2019-11-29 09:08:50 +0300

    Updates to db file

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,1 +1,6 @@
-create schema mas;
+# create databases
+CREATE DATABASE IF NOT EXISTS `akilimo_db2`;
+CREATE DATABASE IF NOT EXISTS `akilimo_site`;
+
+# create root user and grant rights
+GRANT ALL PRIVILEGES ON *.* TO 'akilimo'@'%';
