5f2de1476606be342e8e491ec50acb65b69f8f94
Tsobu Developer <dev@tsobu.co.ke>
2021-02-22 10:56:16 +0300

    Added open km image

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,7 +1,9 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `akilimo_db_test`;
 CREATE DATABASE IF NOT EXISTS `akilimo_site`;
+CREATE DATABASE IF NOT EXISTS `okmdb` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
 
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON akilimo_site.* TO 'akilimo'@'%';
 GRANT ALL PRIVILEGES ON akilimo_db_test.* TO 'akilimo'@'%';
+GRANT ALL PRIVILEGES ON okmdb.* TO 'akilimo'@'%';
