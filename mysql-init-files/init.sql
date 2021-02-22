64af594c77fe2615161adb160d6488eb912fddc3
Tsobu Developer <dev@tsobu.co.ke>
2021-02-22 12:00:10 +0300

    Updates to sql script

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,9 +1,12 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `akilimo_db_test`;
 CREATE DATABASE IF NOT EXISTS `akilimo_site`;
-CREATE DATABASE IF NOT EXISTS `okmdb` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
+CREATE DATABASE IF NOT EXISTS `akilimo_dms` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
 
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON akilimo_site.* TO 'akilimo'@'%';
 GRANT ALL PRIVILEGES ON akilimo_db_test.* TO 'akilimo'@'%';
-GRANT ALL PRIVILEGES ON okmdb.* TO 'akilimo'@'%';
+GRANT ALL PRIVILEGES ON akilimo_dms.* TO 'akilimo'@'%';
+
+DROP SCHEMA `akilimo_site`;
+DROP SCHEMA  `akilimo_db_test`;
