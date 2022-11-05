287cd57f36b5cda34fc2072b4789f6e92d0bdb29
Sammy M <s.barasa@cgiar.org>
2022-11-05 10:09:43 +0300

    Update init.sql

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,12 +1,12 @@
 # create databases
-CREATE DATABASE IF NOT EXISTS `akilimo_db_test`;
+CREATE DATABASE IF NOT EXISTS `akilimo_db`;
 CREATE DATABASE IF NOT EXISTS `akilimo_site`;
 CREATE DATABASE IF NOT EXISTS `akilimo_dms` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_bin;
 CREATE DATABASE IF NOT EXISTS `keycloak`;
 
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON akilimo_site.* TO 'akilimo'@'%';
-GRANT ALL PRIVILEGES ON akilimo_db_test.* TO 'akilimo'@'%';
+GRANT ALL PRIVILEGES ON akilimo_db.* TO 'akilimo'@'%';
 GRANT ALL PRIVILEGES ON akilimo_dms.* TO 'akilimo'@'%';
 GRANT ALL PRIVILEGES ON keycloak.* TO 'akilimo'@'%';
 
